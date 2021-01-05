4ba79bb9a6a768ee87221114be9cdda1240defb0
maxwelljmckee <maxwelljmckee@gmail.com>
2021-01-05 14:54:16 -0700

    configure react app for redux && collect favicon and other images

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import './styles/index.css';
 import App from './App';
 
+import configureStore from './store/index';
+
+
+// CREATE GOLBAL STATE WITH REDUX STORE
+export const store = configureStore();
 
 const Root = () => {
   return (
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <App />
     </Provider>
   )
 }
@@ -18,6 +22,7 @@ const Root = () => {
 ReactDOM.render(
   <React.StrictMode>
     <Root />
+    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ frontend/src/store/session.js
@@ -0,0 +1,75 @@
+
+// DEFINE CONSTANTS //
+const SET_SESSION_USER = 'SET_SESSION_USER';
+const REMOVE_SESSION_USER = 'REMOVE_SESSION_USER';
+
+
+// DEFINE ACTION CREATORS - ASYNC/THUNK //
+export const loginUser = (credential, password) => async (dispatch) => {
+  const res = await fetch('/api/session', {
+    method: 'POST',
+    body: JSON.stringify({ credential, password })
+  })
+  const data = res.json()
+  dispatch(setSessionUser(data.user));
+  return res;
+}
+
+export const registerUser = (user) => async (dispatch) => {
+  const { email, password, username } = user;
+  const res = await fetch('/api/users', {
+    method: 'POST',
+    body: JSON.stringify({ email, password, username })
+  })
+  const data = res.json()
+  dispatch(setSessionUser(data.user));
+  return res;
+}
+
+export const restoreUser = () => async (dispatch) => {
+  const res = await fetch('/api/session');
+  const data = res.json();
+  dispatch(setSessionUser(data.user));
+  return res
+}
+
+export const deleteSession = () => async (dispatch) => {
+  const res = await fetch('/api/session', {
+    method: 'DELETE',
+  })
+  dispatch(removeSessionUser());
+  return res
+}
+
+
+// DEFINE ACTION CREATORS - SYNC //
+export const setSessionUser = (user) => {
+  return {
+    type: SET_SESSION_USER,
+    user
+  }
+}
+
+export const removeSessionUser = () => {
+  return {
+    type: REMOVE_SESSION_USER
+  }
+}
+
+
+// DEFINE REDUCER //
+export const sessionReducer = (state = { user: null }, action) => {
+  let newState;
+  switch (action.type) {
+    case SET_SESSION_USER:
+      newState = { ...state };
+      newState.user = action.user;
+      return newState
+    case REMOVE_SESSION_USER:
+      newState = { ...state };
+      newState.user = null
+      return newState;
+    default:
+      return state;
+  }
+}
