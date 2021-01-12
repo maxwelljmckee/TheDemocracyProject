502b1e4fdaecf42bdf81405506a66d61e1b7377b
maxwelljmckee <maxwelljmckee@gmail.com>
2021-01-11 20:23:50 -0700

    prepare for continued dash setup tomorrow

--- frontend/src/components/Dashboard/DashboardContent.js
+++ frontend/src/components/Dashboard/DashboardContent.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import { useSelector } from 'react-redux';
 
 import SectionBreak from '../Layout/SectionBreak';
 import dashboardIcon from './Dashboard-icon.png';
@@ -6,16 +7,34 @@ import FollowRepsBadges from './FollowRepsBadges';
 
 
 const DashboardContent = ({ user }) => {
+  const [followedReps, setFollowedReps] = useState([]);
+  const [followedBills, setFollowedBills] = useState([]);
+
+  // FETCH FOLLOWED REPS AND STORE IN STATE
+  useEffect(() => {
+    
+  }, [])
+
+  // FETCH FOLLOWED BILLS AND STORE IN STATE
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <div className='dashboard__container slide-in-bottom-rebound' >
       <div className='dashboard__icon-container'>
         <img className='dashboard__icon' src={dashboardIcon} alt='dashboard' />
       </div>
       <SectionBreak sectionTitle='Meet Your Representatives' />
+      {/* for rep in followedReps, map RepCard */}
       <SectionBreak sectionTitle='Follow Other Representatives' />
       <FollowRepsBadges />
-      <SectionBreak sectionTitle='Follow Active Bills' />
+      <SectionBreak sectionTitle="Bills You're Following" />
+      {/* for bill in followedBills, map BillCard */}
+      <SectionBreak sectionTitle='See More Bills' />
+      {/* load 4 unfollowed bills */}
       <SectionBreak sectionTitle='See what people are saying' />
+      {/* Community Forum */}
     </div>
   )
 }
