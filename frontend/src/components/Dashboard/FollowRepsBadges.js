336b13dcbed5910481e30ad83d9aaea882260107
maxwelljmckee <maxwelljmckee@gmail.com>
2021-01-11 18:14:33 -0700

    tweak dashboard rep badges

--- frontend/src/components/Dashboard/FollowRepsBadges.js
+++ frontend/src/components/Dashboard/FollowRepsBadges.js
@@ -4,7 +4,7 @@ import React from 'react';
 const FollowRepsBadges = () => {
   const executiveUrl = 'https://miro.medium.com/max/1200/0*ysaO9dlLm4ElKcMK.png';
   const houseUrl = 'https://radiotv.house.gov/sites/radiotv.house.gov/files/Seal_of_the_United_States_House_of_Representatives.png';
-  const senateUrl = 'https://lh3.googleusercontent.com/proxy/ii5GivgnVABbREtL8EZsF8vcq2m2ajs4UAQwg-pH8bH4jMEmiGccfnWGLbkiOG8B9onwJ1ohtnv56JzHlegZDWEskA';
+  const senateUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/President_Pro_Tempore_US_Senate_Seal.svg/1200px-President_Pro_Tempore_US_Senate_Seal.svg.png';
 
   const handleExecutiveClick = () => {
     
